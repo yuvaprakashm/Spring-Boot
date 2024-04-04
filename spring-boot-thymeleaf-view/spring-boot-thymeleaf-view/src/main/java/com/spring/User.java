66e92cb146457a3320995bf48c10762dfe810656
yuvaprakashm <103112539+yuvaprakashm@users.noreply.github.com>
2024-04-04 16:58:04 +0530

    Add files via upload

---
+++ spring-boot-thymeleaf-view/spring-boot-thymeleaf-view/src/main/java/com/spring/User.java
@@ -0,0 +1,23 @@
+package com.spring;
+
+public class User {
+	String name;
+	String email;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
